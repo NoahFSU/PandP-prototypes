c8f81c1ba85fac93a9236c6ba56fea1dad360b31
NoahFSU <146155809+NoahFSU@users.noreply.github.com>
2024-05-30 02:00:29 -0400

    Enemy spawners created and gamegoal implementation changed

--- Cowboys v Aliens proto1/Assets/Scripts/Spawner.cs
+++ Cowboys v Aliens proto1/Assets/Scripts/Spawner.cs
@@ -19,7 +19,7 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //gameManager.Instance.updateGameGoal(numberToSpawn);
+        gameManager.Instance.updateGameGoal(numberToSpawn);
     }
 
     // Update is called once per frame
