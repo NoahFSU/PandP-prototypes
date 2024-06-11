a3718c2cab18003e5da8d0022880d51ec5000f2c
lucas4356 <39502514+lucas4356@users.noreply.github.com>
2024-06-11 18:46:03 -0400

    breakable cratescript

--- Cowboys v Aliens proto1/Assets/Scripts/WallBreak.cs
+++ Cowboys v Aliens proto1/Assets/Scripts/WallBreak.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WallBreak : MonoBehaviour, IDamage
@@ -7,12 +9,13 @@ public class WallBreak : MonoBehaviour, IDamage
 
     [SerializeField] int WallHP;
     [SerializeField] Renderer WallModel;
+    [SerializeField] List<GameObject> drops = new List<GameObject>();
 
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void TakeDamage(int amount)
@@ -22,6 +25,32 @@ public class WallBreak : MonoBehaviour, IDamage
 
         if (WallHP <= 0)
         {
+            int ran = Random.Range(0, 100);
+            if ( ran > 40 && drops.Count != 0)
+            {
+                if ( ran < 70)
+                {
+                    if ( ran % 2 > 0)
+                    {
+                        GameObject smallH = Instantiate(drops[0], transform.position, transform.rotation);
+                    }
+                    else
+                    {
+                        GameObject smallA = Instantiate(drops[1], transform.position, transform.rotation);
+                    }
+                }
+                else
+                {
+                    if (ran % 2 > 0)
+                    {
+                        GameObject bigH = Instantiate(drops[2], transform.position, transform.rotation);
+                    }
+                    else
+                    {
+                        GameObject bigA = Instantiate(drops[3], transform.position, transform.rotation);
+                    }
+                }
+            }
             Destroy(gameObject);
         }
     }
