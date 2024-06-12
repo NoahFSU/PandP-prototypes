2e08e08068962d24b0fac5f2844f1793c624b315
Lord Savage77 <65084889+RAWBOYCUUL@users.noreply.github.com>
2024-06-11 20:53:10 -0400

    MainMenu

---
+++ Cowboys v Aliens proto1/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        SceneManager.LoadScene("MainMenu");
+#else
+        Application.Quit();
+#endif
+        Debug.Log("Game Closed");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("C_V_A_V1.0.0");
+    }
+}
--- Cowboys v Aliens proto1/Assets/Scripts/buttonFunctions.cs
+++ Cowboys v Aliens proto1/Assets/Scripts/buttonFunctions.cs
@@ -23,11 +23,7 @@ public class buttonFunctions : MonoBehaviour
 
     public void Quit()
     {
-    #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-     #else
-        Application.Quit();
-    #endif
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void LoadLevel(int level)
